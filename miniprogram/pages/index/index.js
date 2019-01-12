6ed0cc2f0b4d8b13b49ffed56060b1ad58aa14d2
brzhang <hoollyzhang@tencent.com>
2019-01-13 00:27:30 +0800

    【新增】添加我的页面

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -5,8 +5,6 @@ const app = getApp()
 let interval = undefined
 Page({
   data: {
-    avatarUrl: './user-unlogin.png',
-    userInfo: {},
     tags: [],
     logged: false,
     takeSession: false,
@@ -26,25 +24,7 @@ Page({
       wx.redirectTo({
         url: '../chooseLib/chooseLib'
       })
-      return
     }
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              this.setData({
-                avatarUrl: res.userInfo.avatarUrl,
-                userInfo: res.userInfo
-              })
-              console.log(res)
-            }
-          })
-        }
-      }
-    })
   },
 
   initData: function () {
---
+++ miniprogram/pages/mine/mine.js
@@ -0,0 +1,67 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    version: '当前版本：版本1.1.0',
+    desc: '随心，随意，快乐生活~',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  onLoad: function () {
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              this.setData({
+                // avatarUrl: res.userInfo.avatarUrl,
+                userInfo: res.userInfo,
+                hasUserInfo: true
+              })
+              console.log(res)
+            }
+          })
+        }
+      }
+    })
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function (e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
